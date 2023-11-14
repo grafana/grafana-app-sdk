ac7db84ec23626bd341a5620d4b80b0ffee6f791
Austin Pond <austin.pond@grafana.com>
2023-11-14 10:42:43 -0500

    Improvements to simple.Operator configuration, added telemetry.

--- go.mod
+++ go.mod
@@ -121,6 +121,7 @@ require (
 	go.opentelemetry.io/contrib/propagators/jaeger v1.20.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.19.0 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc v1.19.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracehttp v1.19.0 // indirect
 	go.opentelemetry.io/otel/metric v1.19.0 // indirect
 	go.opentelemetry.io/otel/sdk v1.19.0 // indirect
 	go.opentelemetry.io/proto/otlp v1.0.0 // indirect
--- simple/operator.go
+++ simple/operator.go
@@ -6,35 +6,104 @@ import (
 	"strings"
 
 	"github.com/grafana/grafana-app-sdk/k8s"
+	"github.com/grafana/grafana-app-sdk/metrics"
 	"github.com/grafana/grafana-app-sdk/operator"
 	"github.com/grafana/grafana-app-sdk/resource"
+	"github.com/prometheus/client_golang/prometheus"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/rest"
 )
 
+// OperatorConfig is used to configure an Operator on creation
+type OperatorConfig struct {
+	Name         string
+	KubeConfig   rest.Config
+	Webhooks     WebhookConfig
+	Metrics      MetricsConfig
+	Tracing      TracingConfig
+	ErrorHandler func(ctx context.Context, err error)
+}
+
+// WebhookConfig is a configuration for exposed kubernetes webhooks for an Operator
 type WebhookConfig struct {
-	Enabled   bool
-	Port      int
+	Enabled bool
+	// Port is the port to open the webhook server on
+	Port int
+	// TLSConfig is the TLS Cert and Key to use for the HTTPS endpoints exposed for webhooks
 	TLSConfig k8s.TLSConfig
+	// DefaultValidator is an optional Default ValidatingAdmissionController to use if a specific one for the incoming
+	// kind cannot be found
+	DefaultValidator resource.ValidatingAdmissionController
+	// DefaultMutator is an optional Default MutatingAdmissionController to use if a specific one for the incoming
+	// kind cannot be found
+	DefaultMutator resource.MutatingAdmissionController
+	// Validators is an optional map of schema => ValidatingAdmissionController to use for the schema on admission.
+	// This can be empty or nil and specific ValidatingAdmissionControllers can be set later with Operator.ValidateKind
+	Validators map[resource.Schema]resource.ValidatingAdmissionController
+	// Mutators is an optional map of schema => MutatingAdmissionController to use for the schema on admission.
+	// This can be empty or nil and specific MutatingAdmissionControllers can be set later with Operator.MutateKind
+	Mutators map[resource.Schema]resource.MutatingAdmissionController
+	// Converters is an optional map of GroupKind => Converter to use for CRD version conversion requests.
+	// This can be empty or nil and specific MutatingAdmissionControllers can be set later with Operator.MutateKind
+	Converters map[metav1.GroupKind]k8s.Converter
+}
+
+// MetricsConfig contains configuration information for exposing prometheus metrics
+type MetricsConfig struct {
+	metrics.ExporterConfig
+	Enabled   bool
+	Namespace string
+}
+
+// TracingConfig contains configuration information for OpenTelemetry tracing
+type TracingConfig struct {
+	Enabled bool
+	OpenTelemetryConfig
 }
 
 // NewOperator creates a new Operator
-func NewOperator(kubeConfig rest.Config, webhookConfig WebhookConfig) (*Operator, error) {
-	cg := k8s.NewClientRegistry(kubeConfig, k8s.ClientConfig{})
+func NewOperator(cfg OperatorConfig) (*Operator, error) {
+	cg := k8s.NewClientRegistry(cfg.KubeConfig, k8s.ClientConfig{})
 	var ws *k8s.WebhookServer
-	if webhookConfig.Enabled {
+	if cfg.Webhooks.Enabled {
 		var err error
 		ws, err = k8s.NewWebhookServer(k8s.WebhookServerConfig{
-			Port:      0,
-			TLSConfig: k8s.TLSConfig{},
+			Port:                        cfg.Webhooks.Port,
+			TLSConfig:                   cfg.Webhooks.TLSConfig,
+			DefaultValidatingController: cfg.Webhooks.DefaultValidator,
+			DefaultMutatingController:   cfg.Webhooks.DefaultMutator,
+			ValidatingControllers:       cfg.Webhooks.Validators,
+			MutatingControllers:         cfg.Webhooks.Mutators,
+			KindConverters:              cfg.Webhooks.Converters,
 		})
 		if err != nil {
 			return nil, err
 		}
 	}
+
+	informerControllerConfig := operator.DefaultInformerControllerConfig()
+	informerControllerConfig.MetricsConfig.Namespace = cfg.Metrics.Namespace
+	// TODO: other factors?
+	controller := operator.NewInformerController(informerControllerConfig)
+
+	// Telemetry (metrics, traces)
+	var me *metrics.Exporter
+	if cfg.Metrics.Enabled {
+		me = metrics.NewExporter(cfg.Metrics.ExporterConfig)
+		me.RegisterCollectors(cg.PrometheusCollectors()...)
+		me.RegisterCollectors(controller.PrometheusCollectors()...)
+	}
+	if cfg.Tracing.Enabled {
+		SetTraceProvider(cfg.Tracing.OpenTelemetryConfig)
+	}
+
 	return &Operator{
-		clientGen:  cg,
-		controller: operator.NewInformerController(),
-		admission:  ws,
+		Name:            cfg.Name,
+		ErrorHandler:    cfg.ErrorHandler,
+		clientGen:       cg,
+		controller:      controller,
+		admission:       ws,
+		metricsExporter: me,
 	}, nil
 }
 
@@ -43,10 +112,14 @@ func NewOperator(kubeConfig rest.Config, webhookConfig WebhookConfig) (*Operator
 // ReconcileKind to add a reconciler for a specific kind (schema) and configuration (such as namespace, label filers),
 // and ValidateKind or MutateKind to add admission control for a kind (schema).
 type Operator struct {
-	Name       string
-	clientGen  resource.ClientGenerator
-	controller *operator.InformerController
-	admission  *k8s.WebhookServer
+	Name string
+	// ErrorHandler, if non-nil, is called when a recoverable error is encountered in underlying components.
+	// This is typically used for logging and/or metrics.
+	ErrorHandler    func(ctx context.Context, err error)
+	clientGen       resource.ClientGenerator
+	controller      *operator.InformerController
+	admission       *k8s.WebhookServer
+	metricsExporter *metrics.Exporter
 }
 
 type ListWatchOptions struct {
@@ -61,19 +134,39 @@ type SyncWatcher interface {
 	Sync(context.Context, resource.Object) error
 }
 
+// ClientGenerator returns the ClientGenerator used by the Operator for getting clients for a particular schema
 func (o *Operator) ClientGenerator() resource.ClientGenerator {
 	return o.clientGen
 }
 
+// Run will start the operator and run until stopCh is closed or receives message.
+// While running, the operator will:
+//
+// * Watch/Reconcile all configured resources
+//
+// * Expose all configured webhooks as an HTTPS server
+//
+// * Expose a prometheus metrics endpoint if configured
 func (o *Operator) Run(stopCh <-chan struct{}) error {
 	op := operator.New()
 	op.AddController(o.controller)
 	if o.admission != nil {
 		op.AddController(o.admission)
 	}
+	if o.metricsExporter != nil {
+		op.AddController(o.metricsExporter)
+	}
 	return op.Run(stopCh)
 }
 
+// RegisterMetricsCollectors registers Prometheus collectors with the exporter used by the operator,
+// and will expose those metrics via the metrics endpoint configured in the operator config on Operator.Run
+func (o *Operator) RegisterMetricsCollectors(collectors ...prometheus.Collector) error {
+	return o.metricsExporter.RegisterCollectors(collectors...)
+}
+
+// WatchKind will watch the specified kind (schema) with opinionated logic, passing the relevant events on to the SyncWatcher.
+// You can configure the query used for watching the kind using ListWatchOptions.
 func (o *Operator) WatchKind(schema resource.Schema, watcher SyncWatcher, options ListWatchOptions) error {
 	client, err := o.clientGen.ClientFor(schema)
 	if err != nil {
@@ -141,6 +234,17 @@ func (o *Operator) MutateKind(schema resource.Schema, controller resource.Mutati
 		return fmt.Errorf("webhooks are not enabled")
 	}
 	o.admission.AddMutatingAdmissionController(controller, schema)
+	return nil
+}
+
+func (o *Operator) ConvertKind(schema resource.Schema, converter k8s.Converter) error {
+	if o.admission == nil {
+		return fmt.Errorf("webhooks are not enabled")
+	}
+	o.admission.AddConverter(converter, metav1.GroupKind{
+		Group: schema.Group(),
+		Kind:  schema.Kind(),
+	})
 	return nil
 }
 
