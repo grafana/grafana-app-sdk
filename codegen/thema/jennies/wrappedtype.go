4755dcb7bd1a8013d991e6510c510f978b42b081
Austin Pond <IfSentient@users.noreply.github.com>
2023-11-23 08:57:36 -0500

    [codegen] Consistent Generated Model Naming (#162)
    Use sanitized exported kind name as go type for models, instead of using
    exported machine name. This is consistent with the previous thema
    codegen for model types.

--- codegen/thema/jennies/wrappedtype.go
+++ codegen/thema/jennies/wrappedtype.go
@@ -4,9 +4,9 @@ import (
 	"bytes"
 	"fmt"
 	"go/format"
+	"strings"
 
 	"github.com/grafana/codejen"
-
 	"github.com/grafana/grafana-app-sdk/codegen"
 	"github.com/grafana/grafana-app-sdk/codegen/templates"
 )
@@ -20,7 +20,7 @@ func (*ModelsFunctionsGenerator) JennyName() string {
 
 func (s *ModelsFunctionsGenerator) Generate(kind codegen.Kind) (*codejen.File, error) {
 	meta := kind.Properties()
-	typeName := exportField(meta.Kind)
+	typeName := exportField(sanitizeLabelString(meta.Kind))
 	md := templates.WrappedTypeMetadata{
 		Package:     meta.MachineName,
 		TypeName:    typeName,
@@ -37,4 +37,21 @@ func (s *ModelsFunctionsGenerator) Generate(kind codegen.Kind) (*codejen.File, e
 		return nil, err
 	}
 	return codejen.NewFile(fmt.Sprintf("%s/%s_marshal_gen.go", meta.MachineName, meta.MachineName), formatted, s), nil
+}
+
+func sanitizeLabelString(s string) string {
+	return strings.Map(func(r rune) rune {
+		switch {
+		case r >= 'a' && r <= 'z':
+			fallthrough
+		case r >= 'A' && r <= 'Z':
+			fallthrough
+		case r >= '0' && r <= '9':
+			fallthrough
+		case r == '_':
+			return r
+		default:
+			return -1
+		}
+	}, s)
 }
