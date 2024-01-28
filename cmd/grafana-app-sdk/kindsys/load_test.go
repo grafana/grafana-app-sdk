7f2f7f1ba165eb4376cbb7a5072e153c53ab82c7
Austin Pond <austin.pond@grafana.com>
2024-01-28 12:57:53 -0500

    Separate into modules for plugin and CLI to avoid versioning issues.

---
+++ cmd/grafana-app-sdk/kindsys/load_test.go
@@ -0,0 +1,12 @@
+package kindsys_test
+
+import (
+	"testing"
+
+	"github.com/grafana/grafana-app-sdk/cmd/grafana-app-sdk/kindsys"
+)
+
+func TestFramework(t *testing.T) {
+	// please don't panic, that's all I ask
+	_ = kindsys.CUEFramework(nil)
+}
