a943aafb6a7ba0684d1fc985bd501664d8be6f8d
Austin Pond <IfSentient@users.noreply.github.com>
2025-01-07 08:17:49 -0500

    Deprecate `simple.NewOperator` and update examples (#565)
    Added deprecation comment to `simple.NewOperator` and `simple.Operator`.
    `examples/operator/simple` was still using `simple.Operator`, so the
    code has been updated to use `simple.App` and `operator.Runner`.
    `examples/operator/opinionated` was building an operator from scratch,
    which is no longer advised, so the example has been removed.

--- simple/operator.go
+++ simple/operator.go
@@ -73,6 +73,7 @@ type TracingConfig struct {
 }
 
 // NewOperator creates a new Operator
+// Deprecated: please use simple.NewApp and operator.NewRunner to create a simple operator app.
 func NewOperator(cfg OperatorConfig) (*Operator, error) {
 	cg := k8s.NewClientRegistry(cfg.KubeConfig, k8s.ClientConfig{})
 	var ws *k8s.WebhookServer
@@ -144,6 +145,7 @@ func NewOperator(cfg OperatorConfig) (*Operator, error) {
 // use WatchKind to add a watcher for a specific kind (schema) and configuration (such as namespace, label filters),
 // ReconcileKind to add a reconciler for a specific kind (schema) and configuration (such as namespace, label filers),
 // and ValidateKind or MutateKind to add admission control for a kind (schema).
+// Deprecated: use simple.App in conjunction with operator.Runner instead.
 type Operator struct {
 	Name string
 	// ErrorHandler, if non-nil, is called when a recoverable error is encountered in underlying components.
