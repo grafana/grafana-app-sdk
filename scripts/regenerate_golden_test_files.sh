e47485a41a6c0f9169c3141b8f1e148fd54ee068
Austin Pond <austin.pond@grafana.com>
2025-01-08 17:02:56 -0500

    Update the regenerate test files script, include JSON manifests for CI.

--- scripts/regenerate_golden_test_files.sh
+++ scripts/regenerate_golden_test_files.sh
@@ -8,31 +8,43 @@ testdir="${rootdir}/codegen/testing/golden_generated"
 echo "Regenerating golden test files from current codegen output"
 
 # Group by group
-go run ./cmd/grafana-app-sdk/*.go generate -c="${rootdir}/codegen/cuekind/testing" \
+go run ./cmd/grafana-app-sdk/*.go generate -s="${rootdir}/codegen/cuekind/testing" \
   -g="${testdir}/go/groupbygroup" \
-  --crdpath="${testdir}/crd" \
+  --defpath="${testdir}/crd" \
   -t="${testdir}/typescript/versioned" \
-  --kindgrouping=group --nomanifest \
-  --selectors="customManifest,testManifest"
-go run ./cmd/grafana-app-sdk/*.go generate -c="${rootdir}/codegen/cuekind/testing" \
+  --grouping=group \
+  --manifest="customManifest"
+go run ./cmd/grafana-app-sdk/*.go generate -s="${rootdir}/codegen/cuekind/testing" \
   -g="${testdir}/go/groupbygroup" \
-  --crdpath="${testdir}/crd" \
-  --crdencoding=yaml \
+  --defpath="${testdir}/crd" \
   -t="${testdir}/typescript/versioned" \
-  --kindgrouping=group \
-  --selectors="customManifest,testManifest"
+  --grouping=group \
+  --manifest="testManifest"
+go run ./cmd/grafana-app-sdk/*.go generate -s="${rootdir}/codegen/cuekind/testing" \
+  -g="${testdir}/go/groupbygroup" \
+  --defpath="${testdir}/crd" \
+  --defencoding=yaml \
+  -t="${testdir}/typescript/versioned" \
+  --grouping=group \
+  --manifest="customManifest"
+go run ./cmd/grafana-app-sdk/*.go generate -s="${rootdir}/codegen/cuekind/testing" \
+  -g="${testdir}/go/groupbygroup" \
+  --defpath="${testdir}/crd" \
+  --defencoding=yaml \
+  -t="${testdir}/typescript/versioned" \
+  --grouping=group \
+  --manifest="testManifest"
 # Move the manifest files
-mv "${testdir}/go/groupbygroup/testapp_manifest.go" "${testdir}/manifest/go/testkinds/testapp_manifest.go.txt"
-mv "${testdir}/go/groupbygroup/customapp_manifest.go" "${testdir}/manifest/go/testkinds/customapp_manifest.go.txt"
-mv "${testdir}/crd/test-app-manifest.yaml" "${testdir}/manifest/test-app-manifest.yaml"
-mv "${testdir}/crd/custom-app-manifest.yaml" "${testdir}/manifest/custom-app-manifest.yaml"
+mv ${testdir}/go/groupbygroup/*.go "${testdir}/manifest/go/"
+mv ${testdir}/crd/test-app-manifest.* "${testdir}/manifest/"
+mv ${testdir}/crd/custom-app-manifest.* "${testdir}/manifest/"
 # Group by kind (only customKind)
-go run ./cmd/grafana-app-sdk/*.go generate -c="${rootdir}/codegen/cuekind/testing" \
+go run ./cmd/grafana-app-sdk/*.go generate -s="${rootdir}/codegen/cuekind/testing" \
   -g="${testdir}/go/groupbykind" \
-  --crdpath="${testdir}/crd" \
+  --defencoding="none" \
   -t="${testdir}/typescript/versioned" \
-  --kindgrouping=kind --nomanifest --notypeinpath \
-  --selectors="customManifest"
+  --grouping=kind \
+  --manifest="customManifest"
 
 # Rename files to append .txt
 find "${testdir}" -depth -name "*.go" -exec sh -c 'mv "$1" "${1}.txt"' _ {} \;
