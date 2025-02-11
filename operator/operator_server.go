e0abcd30cacc7f16f9c706573d7dbb562735809f
Charandas Batra <charandas.batra@grafana.com>
2025-02-11 15:24:30 -0800

    address more feedback

--- operator/operator_server.go
+++ operator/operator_server.go
@@ -5,17 +5,24 @@ package operator
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/grafana/grafana-app-sdk/health"
 )
 
 type OperatorServer struct {
-	Mux         *http.ServeMux
-	HealthCheck health.HealthCheck
-	Port        int
+	Checks              []health.Check
+	Mux                 *http.ServeMux
+	Port                int
+	HealthCheckInterval time.Duration
+
+	// access to healthCheckErr is guarded with healthCheckErrMu
+	healthCheckErr   error
+	healthCheckErrMu sync.RWMutex
 }
 
 func (s *OperatorServer) RegisterMetricsHandler(handler http.Handler) {
@@ -27,11 +34,20 @@ func (s *OperatorServer) registerHealthHandlers() error {
 		_, _ = w.Write([]byte("ok"))
 		return
 	}))
-	s.Mux.Handle("/readyz", s.HealthCheck.HTTPHandler())
+	s.Mux.Handle("/readyz", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if err := s.cachedChecks(); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			_, _ = w.Write([]byte("readiness check failed: " + err.Error()))
+			return
+		}
+		_, _ = w.Write([]byte("ok"))
+		return
+	}))
 	return nil
 }
 
-func (s *OperatorServer) Run(context.Context) error {
+func (s *OperatorServer) Run(ctx context.Context) error {
+	wg := &sync.WaitGroup{}
 	// Run creates an HTTP server which exposes
 	// 1. if enabled, a /metrics endpoint on the configured port
 	// 2. health endpoints for liveness and readiness
@@ -44,5 +60,60 @@ func (s *OperatorServer) Run(context.Context) error {
 		Handler:           s.Mux,
 		ReadHeaderTimeout: 5 * time.Second,
 	}
-	return server.ListenAndServe()
+
+	var serverErr error
+	wg.Add(1)
+	go func() {
+		if err := server.ListenAndServe(); !errors.Is(err, http.ErrServerClosed) {
+			serverErr = err
+		}
+		wg.Done()
+	}()
+
+	// hydrate the result on startup, so that we have something set before the periodic checks start happening
+	if err := s.runChecks(ctx); err != nil {
+		return err
+	}
+	wg.Add(1)
+	go func() {
+		for {
+			select {
+			case <-time.After(s.HealthCheckInterval):
+				_ = s.runChecks(ctx)
+			case <-ctx.Done():
+				_ = server.Shutdown(ctx)
+				wg.Done()
+			}
+
+		}
+	}()
+
+	wg.Wait()
+	return serverErr
+}
+
+// HTTPHandler returns an http.Handler that performs the health checks
+func (s *OperatorServer) cachedChecks() error {
+	s.healthCheckErrMu.RLock()
+	defer s.healthCheckErrMu.RUnlock()
+
+	return s.healthCheckErr
+}
+
+func (s *OperatorServer) runChecks(ctx context.Context) error {
+	s.healthCheckErrMu.Lock()
+	defer s.healthCheckErrMu.Unlock()
+
+	fmt.Println("Running health check...")
+
+	var allErrors error
+	for _, check := range s.Checks {
+		if err := check.HealthCheck(ctx); err != nil {
+			allErrors = errors.Join(allErrors, err)
+		}
+	}
+	if allErrors != nil {
+		s.healthCheckErr = allErrors
+	}
+	return nil
 }
