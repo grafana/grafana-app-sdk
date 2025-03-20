c3a393cef3cd7c3ba5f6caa392706fb68d16dbc6
Charandas Batra <charandas.batra@grafana.com>
2025-03-20 11:52:33 -0700

    pair with Austin

--- health/health.go
+++ health/health.go
@@ -4,11 +4,13 @@ import (
 	"context"
 )
 
+// Check is an interface that describes anything that has a health check for it
 type Check interface {
 	HealthCheck(context.Context) error
 	HealthCheckName() string
 }
 
+// Checker
 type Checker interface {
 	// HealthChecks runs all registered health checks and returns any errors combined into a single error
 	HealthChecks() []Check
