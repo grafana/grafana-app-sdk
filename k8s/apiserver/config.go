163cf629cc0b3fff20eb0cc9f1148f17b41bbcf9
Austin Pond <austin.pond@grafana.com>
2025-05-21 13:56:02 -0400

    Set OpenAPI and EffectiveVersion before completing config.

--- k8s/apiserver/config.go
+++ k8s/apiserver/config.go
@@ -3,10 +3,13 @@ package apiserver
 import (
 	"maps"
 
+	"github.com/grafana/grafana-app-sdk/app"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/runtime/serializer"
 	"k8s.io/apiserver/pkg/endpoints/openapi"
 	genericapiserver "k8s.io/apiserver/pkg/server"
+	"k8s.io/apiserver/pkg/util/compatibility"
 	"k8s.io/kube-openapi/pkg/common"
 )
 
@@ -60,12 +63,39 @@ func (c *Config) UpdateOpenAPIConfig() {
 }
 
 func (c *Config) NewServer(delegate genericapiserver.DelegationTarget) (*genericapiserver.GenericAPIServer, error) {
+	manifests := make([]app.ManifestData, 0)
 	for _, installer := range c.installers {
+		manifests = append(manifests, installer.appConfig.ManifestData)
 		_, err := installer.App(*c.Generic.LoopbackClientConfig)
 		if err != nil {
 			return nil, err
 		}
 	}
+	c.Generic.OpenAPIConfig = genericapiserver.DefaultOpenAPIConfig(c.openAPIDefinitionGetterFunc(manifests), openapi.NewDefinitionNamer(c.scheme))
+	c.Generic.OpenAPIV3Config = genericapiserver.DefaultOpenAPIV3Config(c.openAPIDefinitionGetterFunc(manifests), openapi.NewDefinitionNamer(c.scheme))
+	c.Generic.EffectiveVersion = compatibility.DefaultBuildEffectiveVersion()
 	completedConfig := c.Generic.Complete()
 	return completedConfig.New("grafana-app-sdk", delegate)
+}
+
+func (c *Config) openAPIDefinitionGetterFunc(manifests []app.ManifestData) func(callback common.ReferenceCallback) map[string]common.OpenAPIDefinition {
+	return func(callback common.ReferenceCallback) map[string]common.OpenAPIDefinition {
+		res := make(map[string]common.OpenAPIDefinition)
+		for _, m := range manifests {
+			for _, version := range m.Versions {
+				for _, kind := range version.Kinds {
+					oapi, err := kind.Schema.AsKubeOpenAPI(schema.GroupVersionKind{
+						Group:   m.Group,
+						Version: version.Name,
+						Kind:    kind.Kind,
+					}, callback)
+					if err != nil {
+						continue
+					}
+					maps.Copy(res, oapi)
+				}
+			}
+		}
+		return res
+	}
 }
