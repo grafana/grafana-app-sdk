718affb2543b72a2da8a0777b4c670aff11802b8
Igor Suleymanov <igor.suleymanov@grafana.com>
2025-08-11 10:11:13 +0300

    Add custom implementation of cache controller and reflector
    What
    
    This commit adds customized implementations of cache controller &
    reflector from the client-go codebase, that stops using ListWithAlloc
    when listing objects via the pager.
    
    Why
    
    Using allocs leaves duplicate copies of listed objects lying around,
    which makes memory usage grow indefinitely.
    
    Signed-off-by: Igor Suleymanov <igor.suleymanov@grafana.com>

--- operator/informer_customcache.go
+++ operator/informer_customcache.go
@@ -17,6 +17,7 @@ import (
 	"k8s.io/apimachinery/pkg/watch"
 	"k8s.io/client-go/tools/cache"
 
+	customcache "github.com/grafana/grafana-app-sdk/k8s/cache"
 	"github.com/grafana/grafana-app-sdk/logging"
 	"github.com/grafana/grafana-app-sdk/metrics"
 	"github.com/grafana/grafana-app-sdk/resource"
@@ -162,7 +163,13 @@ func (c *CustomCacheInformer) Run(ctx context.Context) error {
 		c.startedLock.Lock()
 		defer c.startedLock.Unlock()
 
-		c.controller = newInformer(c.listerWatcher, c.objectType, c.opts.CacheResyncInterval, c, c.store, nil)
+		c.controller = newInformer(
+			c.listerWatcher,
+			c.objectType,
+			c.schema.GroupVersionKind().String(),
+			c.opts.CacheResyncInterval,
+			c,
+			c.store, nil)
 		c.started = true
 	}()
 
@@ -413,6 +420,7 @@ func toResourceEventHandlerFuncs(
 func newInformer(
 	lw cache.ListerWatcher,
 	objType runtime.Object,
+	objDesc string,
 	resyncPeriod time.Duration,
 	h cache.ResourceEventHandler,
 	clientState cache.Store,
@@ -428,10 +436,11 @@ func newInformer(
 	})
 
 	cfg := &cache.Config{
-		Queue:            fifo,
-		ListerWatcher:    lw,
-		ObjectType:       objType,
-		FullResyncPeriod: resyncPeriod,
+		Queue:             fifo,
+		ListerWatcher:     lw,
+		ObjectType:        objType,
+		ObjectDescription: objDesc,
+		FullResyncPeriod:  resyncPeriod,
 
 		Process: func(obj any, isInInitialList bool) error {
 			if deltas, ok := obj.(cache.Deltas); ok {
@@ -440,7 +449,8 @@ func newInformer(
 			return errors.New("object given as Process argument is not Deltas")
 		},
 	}
-	return cache.New(cfg)
+
+	return customcache.NewController(cfg)
 }
 
 // processDeltas is mostly copied from the kubernetes method of the same name in client-go/tools/cache/controller.go,
