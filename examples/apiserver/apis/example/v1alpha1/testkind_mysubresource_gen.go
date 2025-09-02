d66e0724248bd3ff2e2ab8c08107d1337e390a98
Austin Pond <austin.pond@grafana.com>
2025-09-02 16:11:12 -0400

    [apiserver] Support arbitrary subresources in storage which are declared as part of the schema of the kind and not a subresource route.

---
+++ examples/apiserver/apis/example/v1alpha1/testkind_mysubresource_gen.go
@@ -0,0 +1,13 @@
+// Code generated - EDITING IS FUTILE. DO NOT EDIT.
+
+package v1alpha1
+
+// +k8s:openapi-gen=true
+type TestKindMysubresource struct {
+	ExtraValue string `json:"extraValue"`
+}
+
+// NewTestKindMysubresource creates a new TestKindMysubresource object.
+func NewTestKindMysubresource() *TestKindMysubresource {
+	return &TestKindMysubresource{}
+}
