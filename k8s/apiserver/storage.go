66e42e1b2749ed4c1fe0557cf5d294c2d8e3daf3
Austin Pond <IfSentient@users.noreply.github.com>
2025-09-02 12:12:24 -0400

    k8s.io dependency update fieldpath fix in k8s/apiserver package (#951)
    Dependency update of k8s.io/apiserver from 0.33.4 to 0.34.0 changes
    v4/fieldpath to v6/fieldpath for storage stategies. This needed to be
    updated in the k8s/apiserver package as well, which could not be done
    automagically by dependabot.
    
    Blocks [this dependabot
    PR](https://github.com/grafana/grafana-app-sdk/pull/950)

--- k8s/apiserver/storage.go
+++ k8s/apiserver/storage.go
@@ -12,7 +12,7 @@ import (
 	genericregistry "k8s.io/apiserver/pkg/registry/generic/registry"
 	"k8s.io/apiserver/pkg/registry/rest"
 	"k8s.io/apiserver/pkg/storage"
-	"sigs.k8s.io/structured-merge-diff/v4/fieldpath"
+	"sigs.k8s.io/structured-merge-diff/v6/fieldpath"
 
 	"github.com/grafana/grafana-app-sdk/resource"
 )
