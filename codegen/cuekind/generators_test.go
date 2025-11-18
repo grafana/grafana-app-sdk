709ce5a7b6ea4c4f0aed14fc5ee04ce221f70cb7
Alejandro Malavet <alejandro.malavet@grafana.com>
2025-11-18 11:33:45 -0500

    remove test changes

--- codegen/cuekind/generators_test.go
+++ codegen/cuekind/generators_test.go
@@ -2,7 +2,6 @@ package cuekind
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 	"path/filepath"
 	"testing"
@@ -152,9 +151,7 @@ func TestManifestGoGenerator(t *testing.T) {
 		// 10 -> manifest file, then the custom route response+query+body for reconcile, response body and wrapper+query+body for search in v3, +1 client per version (3)
 		require.Len(t, files, 11, "should be 11 files generated, got %d", len(files))
 		// Check content against the golden files
-		for i, file := range files {
-			err := os.WriteFile(fmt.Sprintf("file_%d", i)+".txt", file.Data, 0644)
-			require.Nil(t, err)
+		for _, file := range files {
 			if file.RelativePath == "testapp_manifest.go" {
 				compareToGolden(t, codejen.Files{file}, "manifest/go/groupbygroup")
 			} else {
