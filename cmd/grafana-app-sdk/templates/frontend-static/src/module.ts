b4ff82ed81a5f1eca8866a718741de1c299f8c16
Austin Pond <IfSentient@users.noreply.github.com>
2025-12-15 15:38:46 -0500

    [cli] Remove `frontend-static` templates (#1116)
    ## What Changed? Why?
    
    Removed the `templates/frontend-static` templates from the CLI as they
    are no longer used after migrating to use `yarn` to create the plugin
    frontend on `project componend add frontend`
    
    ### How was it tested?
    
    Created a new project and added a frontend and backend to ensure that
    nothing broke and the frontend was still created.
    
    ### Where did you document your changes?
    
    N/A - these files were unused
    
    ### Notes to Reviewers

--- cmd/grafana-app-sdk/templates/frontend-static/src/module.ts
+++
@@ -1,5 +0,0 @@
-import { AppPlugin, AppRootProps } from '@grafana/data';
-import { ComponentClass } from 'react';
-import { App } from './App';
-
-export const plugin = new AppPlugin().setRootPage(App as unknown as ComponentClass<AppRootProps>);
